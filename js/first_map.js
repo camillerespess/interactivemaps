f316116cbe0b6ea3919ce83cd835b289c3bee1df
Camille Respess <camillerespess@ufl.edu>
2019-11-16 09:25:45 -0500

    draft 2

--- js/first_map.js
+++ js/first_map.js
@@ -10,7 +10,7 @@ let patticakes = L.marker([29.607327, -82.438420]).addTo(firstmap);
 let opus = L.marker([29.650572, -82.333426]).addTo(firstmap);
 let wyatts = L.marker([29.650560, -82.323694]).addTo(firstmap);
 
-coffeeculture.bindPopup("Coffee Culture");
-patticakes.bindPopup("Patticakes in the Village");
-opus.bindPopup("Opus");
-wyatts.bindPopup("Wyatts");
+coffeeculture.bindPopup("Coffee Culture is a lowkey, dimly lit shop that attracts both Gainesville locals and college students. With eclectic decorations and music set to just the right playlist, Coffee Culture is a staple in this town.");
+patticakes.bindPopup("Twenty minutes from UF's campus, Patticakes in the Village is a bright, open-concept establishment that is well worth the drive. With a long coffee menu, sandwiches and even ice cream, Patticakes is a great place to spend the day.");
+opus.bindPopup("Opus recently opened its newest location in the Innovation Center. Their extensive cold brew menu is not for the faint of heart. With large tables and floor to ceiling windows, Opus makes for an ideal study spot.");
+wyatts.bindPopup("Wyatt's is a downtown Gainesville classic. It's small and popular, so scoring a seat there feels like an ultimate win.");
